8a62bee4e13ad4c263811de9ba727542561c597b
Taichi <ichirakumen@gmail.com>
2025-03-29 01:39:36 +0800

    fifth commit

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Barangay Mo</title>
-    <link rel="stylesheet" href="/styles.css">
+    <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -13,62 +13,3 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-    <header>
-        <div class="container-fluid bg-body-tertiary">
-            <nav class="navbar navbar-expand-lg">
-                <div class="container">
-                    <a class="navbar-brand" href="#">
-                        <img src="images/seal.png" alt="Republic Seal of the Philippines" width="36" height="36">
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item ms-3 me-3">
-                                <a class="nav-link active" aria-current="page" href="#">
-                                    Home
-                                </a>
-                            </li>
-                            <li class="nav-item dropdown me-3">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    About
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Mission and Vision</a></li>
-                                    <li><a class="dropdown-item" href="#">Citizens Charter</a></li>
-                                    <li><a class="dropdown-item" href="#">Map</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown me-3">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Services
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Barangay ID</a></li>
-                                    <li><a class="dropdown-item" href="#">Barangay Clearance</a></li>
-                                    <li><a class="dropdown-item" href="#">Certification</a></li>
-                                    <li><a class="dropdown-item" href="#">Business Permit</a></li>
-                                    <li><a class="dropdown-item" href="#">Katarungang Pambarangay</a></li>
-                                    <li><a class="dropdown-item" href="#">Environmental Services</a></li>
-                                </ul>
-                            </li>
-                             <li class="nav-item me-3">
-                                <a class="nav-link " href="#">
-                                    Transparency Seal
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    Contact Us
-                                </a>
-                            </li>
-                        </ul>
-                        <form class="d-flex" role="search">
-                            <input class="form-control custom-input me-2" type="search" placeholder="Search" aria-label="Search">
-                        </form>
-                    </div>
-                </div> 
-            </nav>
-        </div>
-    </header>

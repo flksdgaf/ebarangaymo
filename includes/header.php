d0086f3e32d9cda1347fbed31f1c4bb40a7d1202
Taichi <ichirakumen@gmail.com>
2025-03-29 00:07:21 +0800

    third commit

--- includes/header.php
+++ includes/header.php
@@ -7,6 +7,9 @@
     <link rel="stylesheet" href="/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
@@ -15,19 +18,19 @@
             <nav class="navbar navbar-expand-lg">
                 <div class="container">
                     <a class="navbar-brand" href="#">
-                        <img src="/images/seal.png" alt="Republic Seal of the Philippines" width="36" height="36">
+                        <img src="images/seal.png" alt="Republic Seal of the Philippines" width="36" height="36">
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
+                            <li class="nav-item ms-3 me-3">
                                 <a class="nav-link active" aria-current="page" href="#">
                                     Home
                                 </a>
                             </li>
-                            <li class="nav-item dropdown">
+                            <li class="nav-item dropdown me-3">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     About
                                 </a>
@@ -37,7 +40,7 @@
                                     <li><a class="dropdown-item" href="#">Map</a></li>
                                 </ul>
                             </li>
-                            <li class="nav-item dropdown">
+                            <li class="nav-item dropdown me-3">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Services
                                 </a>
@@ -50,7 +53,7 @@
                                     <li><a class="dropdown-item" href="#">Environmental Services</a></li>
                                 </ul>
                             </li>
-                             <li class="nav-item">
+                             <li class="nav-item me-3">
                                 <a class="nav-link " href="#">
                                     Transparency Seal
                                 </a>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include 'includes/header.php';
 ?>
 
 <div class="container-fluid px-0">
-    <img src="/images/banner.png" alt="Banner" class="img-fluid w-100">
+    <img src="images/banner.png" alt="Banner" class="img-fluid w-100">
 </div>
 
 <div class="container-fluid px-0">
@@ -17,13 +17,13 @@ include 'includes/header.php';
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-        <img src="/images/carousel.png" class="d-block w-100" alt="...">
+        <img src="images/carousel.png" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-        <img src="/images/carousel.png" class="d-block w-100" alt="...">
+        <img src="images/carousel.png" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
-        <img src="/images/carousel.png" class="d-block w-100" alt="...">
+        <img src="images/carousel.png" class="d-block w-100" alt="...">
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -37,14 +37,47 @@ include 'includes/header.php';
     </div>
 </div>
 
-<div class="container">
-    <h1>ABOUT US</h1>
+<!-- <div class="container-fluid px-0">
+    <div class="about-container text-center">
+        <h2 class="about gradient-text">ABOUT US</h2>
+        <p class="w-50 mx-auto text-center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
 
-    <h1>SERVICES</h1>
-
-    <h1>NEW AND UPDATES</h1>
-
+        <div class="row g-2 mt-4 justify-content-center text-center">
+            <div class="col-lg-3 col-md-4 col-sm-6 d-flex justify-content-center">
+                <div class="card-custom p-4">
+                    <h5 class="gradient-text">Mission</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris... View More
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 d-flex justify-content-center">
+                <div class="card-custom p-4">
+                    <h5 class="gradient-text">Vision</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris... View More
+                    </p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 d-flex justify-content-center">
+                <div class="card-custom p-4">
+                    <h5 class="gradient-text">Values</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris... View More
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+<div class="container-fluid px-0">
+    <div class="services-container text-center">
+        <h2 class="services gradient-text">SERVICES</h2>
+    </div>
+</div> -->
 
 
 <!-- <?php include 'includes/footer.php'; ?> -->

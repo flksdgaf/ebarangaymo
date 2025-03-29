b49184ce4feade82896e8ecac3ecbce8419107e0
Taichi <ichirakumen@gmail.com>
2025-03-29 17:39:34 +0800

    sixth commit

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector("#carouselExampleIndicators");
+    const bgContainer = document.querySelector(".carousel-blur-bg");
+
+    function updateBlurredBackground() {
+        let activeSlide = document.querySelector(".carousel-item.active img");
+        if (activeSlide) {
+            bgContainer.style.backgroundImage = `url('${activeSlide.src}')`;
+        }
+    }
+
+    // Update background when the slide changes
+    carousel.addEventListener("slid.bs.carousel", updateBlurredBackground);
+
+    // Set the initial background
+    updateBlurredBackground();
+});

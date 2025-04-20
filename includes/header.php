e4d1021b7f8c450f74769951ccdf3088e7d3245b
flksdgaf <kentgabriel.britos@gmail.com>
2025-04-20 21:24:10 +0800

    weh

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,19 @@
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
-<link rel="stylesheet" href="styles.css">   
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
-<script src="https://kit.fontawesome.com/e30afd7a6b.js" crossorigin="anonymous"></script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Barangay Mo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="about.css">
+    <link rel="stylesheet" href="signinup.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e30afd7a6b.js" crossorigin="anonymous"></script>
+</head>
 
 <!-- NAVIGATION SECTION -->
 <header>
@@ -22,7 +30,7 @@
                 <!-- Left-side links -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item me-3">
-                        <a class="nav-link active" href="userHomepage.php">Home</a>
+                        <a class="nav-link active" href="index.php">Home</a>
                     </li>
                     
                     <!-- About Dropdown -->
@@ -41,7 +49,7 @@
                     <li class="nav-item dropdown me-3">
                         <a class="nav-link dropdown-toggle" href="services.php" id="servicesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Services</a>
                             <ul class="dropdown-menu" aria-labelledby="servicesDropdown">
-                                <li><a class="dropdown-item" href="serviceBarangayID.php">Barangay ID</a></li>
+                                <li><a class="dropdown-item" href="#">Barangay ID</a></li>
                                 <li><a class="dropdown-item" href="#">Barangay Clearance</a></li>
                                 <li><a class="dropdown-item" href="#">Certification</a></li>
                                 <li><a class="dropdown-item" href="#">Business Permit</a></li>
@@ -63,7 +71,3 @@
         </nav>
     </div>
 </header>
-
-
-<!-- Bootstrap JS Bundle placed at the end of the body -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script> -->
